0395ecca7cb0588474f31d3690116bbfac410e89
unknown <msdcr88@gmail.com>
2022-07-07 21:53:04 +0530

    3 components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { ViewfoodComponent } from './viewfood/viewfood.component';
+import { SearchRestaurantComponent } from './search-restaurant/search-restaurant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    ViewfoodComponent,
+    SearchRestaurantComponent
   ],
   imports: [
     BrowserModule,
